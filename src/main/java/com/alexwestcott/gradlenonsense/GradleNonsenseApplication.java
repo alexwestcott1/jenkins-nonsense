ee5c8374e5f6de46d1f456bc1a824aa990cb1d60
Alex Westcott <alex.westcott@thoughtworks.com>
2020-12-11 12:30:15 +0000

    initial commit

---
+++ src/main/java/com/alexwestcott/gradlenonsense/GradleNonsenseApplication.java
@@ -0,0 +1,13 @@
+package com.alexwestcott.gradlenonsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleNonsenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleNonsenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexwestcott/gradlenonsense/GradleNonsenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexwestcott.gradlenonsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleNonsenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
